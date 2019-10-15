b3bb174279b0aebcb41dae90434d794183515abb
swelchm <stephencwelch@gmail.com>
2019-10-15 10:42:23 -0400

    rough eval script done

--- challenge/eval.py
+++ challenge/eval.py
@@ -6,6 +6,7 @@
 ## ---------------------------- ##
 
 import numpy as np
+import time
 from fastai.vision import *
 from pathlib import Path
 from sample_student import Model
@@ -34,12 +35,60 @@ def evaluate(data_path='../data/bbc_train',
 
 
     print("2. Instantiating Model...")
-    M = Model(path='../models', file='export.pkl'))
+    M = Model(path=model_dir, file='export.pkl'))
+
 
     print("3. Evaluating on Test Images...")
+    x,y = data.one_batch()
+    #Extract class label from mask:
+	class_labels=torch.tensor([np.unique(y[i][y[i]!=0])[0] for i in range(x.shape[0])])
+
+	#Extract bounding box from mask:
+	bboxes=torch.zeros(x.shape[0], 4)
+	for i in range(x.shape[0]):
+	    rows,cols=torch.where(y[i, 0]!=0)
+	    bboxes[i, :] = torch.tensor([rows.min(), cols.min(), rows.max(), cols.max()])
+
+	preds=M.predict(x)
+
+	classification_accuracy=(np.array([data.classes[i] for i in class_labels])==np.array(preds[0])).sum()/len(preds[0])
+	bbox_score = 1 - np.mean(np.abs(np.array(bboxes)-preds[1]))/(x.shape[-1]) #Divide by image height to rougly normalize score
+	segmentation_accuracy=float((preds[2] == np.array(y.squeeze(1))).sum())/y.numel()
+	combined_accuracy=np.mean([classification_accuracy, bbox_score, segmentation_accuracy])
+
+    return combined_accuracy, classification_accuracy, bbox_score, segmentation_accuracy
+
+
+ def calculate_score(combined_accuracy):
+    if combined_accuracy >= 0.99: score = 10
+    elif combined_accuracy >= 0.98: score = 9
+    elif combined_accuracy >= 0.97: score = 8
+    elif combined_accuracy >= 0.96: score = 7
+    elif combined_accuracy >= 0.95: score = 6
+    elif combined_accuracy >= 0.90: score = 5
+    else: score = 4
+    return score
+
+
+if __name__ == '__main__':
+    program_start = time.time()
+    combined_accuracy, classification_accuracy, bbox_score, segmentation_accuracy = evaluate()
+    score = calculate_score(combined_accuracy)
+
+    program_end = time.time()
+    total_time = round(program_end - program_start, 3)
+    
+   	print("Done!")
+    print("Execution time (seconds) = ", total_time)
+    print("Score = ", score, 
+    	 "Combined accuracy = ", combined_accuracy, 
+    	 "Classification accuracy = ", classification_accuracy,
+    	 "bbox score = ", bbox_score,
+    	 "Segmentation Accuracy = ", segmentation_accuracy)
 
 
 
 
 
-    return accuracy
+
+
--- challenge/sample_student.py
+++ challenge/sample_student.py
@@ -5,6 +5,7 @@
 ## You are free to use fastai, pytorch, opencv, and numpy for this challenge.
 ##
 ## Requirements:
+## 0. Make sure your fastai model file is named export.pkl
 ## 1. Your code must be able to run on CPU or GPU.
 ## 2. Must handle different image sizes. 
 ## 3. Use a single unified pytorch model for all 3 tasks. 
@@ -25,11 +26,11 @@ class Model(object):
 		Input: x = block of input images, stored as Torch.Tensor of dimension (batch_sizex3xHxW), 
 				   scaled between 0 and 1. 
 		Returns: a tuple containing: 
-			1. The final class prediction for the image (brick, ball, or cylinder) as a string.
+			1. The final class predictions for each image (brick, ball, or cylinder) as a list of strings.
 			2. Upper left and lower right bounding box coordinates (in pixels) for the brick ball 
-			or cylinder, as a 1d numpy array of length 4. 
-			3. Segmentation mask for the image, as a 2d numpy array of dimension (HxW). Each value 
-			in the segmentation mask should be either 0, 1, 2, or 3. Where 0=background, 1=brick, 
+			or cylinder in each image, as a 2d numpy array of dimension batch_size x 4.
+			3. Segmentation mask for the image, as a 3d numpy array of dimension (batch_sizexHxW). Each value 
+			in each segmentation mask should be either 0, 1, 2, or 3. Where 0=background, 1=brick, 
 			2=ball, 3=cylinder. 
 		'''
 
@@ -48,17 +49,17 @@ class Model(object):
 
 
 		#Random Selection Placeolder Code
-		class_prediction=self.class_names[np.random.randint(3)]
+		class_predictions=[self.class_names[np.random.randint(3)] for i in range(x.shape[0])]
 
 		#Scale randomly chosen bbox coords to image shape:
-		bbox=np.random.rand(4)
-		bbox[0] *= x.shape[2]; bbox[2] *= x.shape[2] 
-		bbox[1] *= x.shape[3]; bbox[3] *= x.shape[3]
+		bbox=np.random.rand(x.shape[0], 4)
+		bbox[:,0] *= x.shape[2]; bbox[:,2] *= x.shape[2] 
+		bbox[:,1] *= x.shape[3]; bbox[:,3] *= x.shape[3]
 
 		#Create random segmentation mask:
-		mask=np.random.randint(low=0, high=4, size=(x.shape[0], x.shape[1]))
+		mask=np.random.randint(low=0, high=4, size=(x.shape[0], x.shape[2], x.shape[3]))
 
-		return (class_prediction, bbox, mask)
+		return (class_predictions, bbox, mask)
 
 
 
